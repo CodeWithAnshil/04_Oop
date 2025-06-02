9c1ab812a33aaa994c63144116ffa3826fbbd814
CodeWithAnshil <anshilmaurya888@gmail.com>
2025-06-02 17:06:17 +0530

    added concept of polymorphisam

---
+++ 04-Polymorphisam.py
@@ -0,0 +1,37 @@
+class Car:
+   def __init__(self,brand,model):
+       self.__brand=brand
+       self.__model=model
+   def full_name(self):
+       return f"{self.__brand} {self.__model}"
+              
+   def get_brand(self):
+       return self.__brand
+   def get_model(self):
+       return self.__model
+   def fule_type(self):
+         return "Disal or Petrol"
+   
+class ElectricCar(Car):
+     def __init__(self,brand,model,battery_size):
+         super().__init__(brand,model)
+         self.__battery_size=battery_size
+
+     def fule_type(self):
+         return "Electric"
+
+
+my_car=Car("Toyota","Corolla")
+Car("Rang Rover","Defendar")
+my_electriCar=ElectricCar("Tesla","model S","88kwh")
+
+print(my_car.get_brand())
+print(my_car.get_model())
+print(my_car.full_name())
+print(my_car.fule_type())
+print("----------------------------------")
+print(my_electriCar.fule_type())
+print(my_electriCar.get_brand())
+print(my_electriCar.get_model())
+print(my_electriCar.full_name())
+# print(my_car.__brand)  it will show AttributeError : 'Car' object has no attribute '__brand'
