c3ae8af29e7d736044d161fddb68118ff78b3230
CodeWithAnshil <anshilmaurya888@gmail.com>
2025-06-02 19:03:44 +0530

    using the concept of @staticmethod decorator

---
+++ 05-Static_Method.py
@@ -0,0 +1,50 @@
+class Car:
+   total_car=0
+   def __init__(self,brand,model):
+       self.__brand=brand
+       self.__model=model
+       Car.total_car+=1
+   def full_name(self):
+       return f"{self.__brand} {self.__model}"
+              
+   def get_brand(self):
+       return self.__brand
+   def get_model(self):
+       return self.__model
+   def fule_type(self):
+         return "Disal or Petrol"
+     #  define a method inside a class that does not access or modify the instance (self) or class (cls).
+   @staticmethod
+   def general_info():
+        return "Cars run on petrol, diesel, or electricity."
+   
+class ElectricCar(Car):
+     total_car=0
+     def __init__(self,brand,model,battery_size):
+         super().__init__(brand,model)
+         self.__battery_size=battery_size
+         ElectricCar.total_car+=1
+
+     def fule_type(self):
+         return "Electric"
+    
+   
+
+
+my_car=Car("Toyota","Corolla")
+Car("Rang Rover","Defendar")
+my_electriCar=ElectricCar("Tesla","model S","88kwh")
+
+# print(my_car.get_brand())
+# print(my_car.get_model())
+# print(my_car.full_name())
+# print(my_car.fule_type())
+# print("----------------------------------")
+# print(my_electriCar.fule_type())
+# print(my_electriCar.get_brand())
+# print(my_electriCar.get_model())
+# print(my_electriCar.full_name())
+# print(ElectricCar.total_car)
+# print(Car.total_car)
+print(Car.general_info())
+# print(my_car.__brand)  it will show AttributeError : 'Car' object has no attribute '__brand'
